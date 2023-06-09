ded405d46c1dfbf9e31dce54af49bd8d7b1d2480
Julia <juliafoerster86@googlemail.com>
2023-06-09 21:57:46 +0100

    chore: completed exercise errors1 to errors4

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -17,16 +17,19 @@
 // one is a lot shorter!
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
+// 2. solution (remove ? in line 27)
+//    match qty{
+//        Ok(quantity) => Ok(quantity * cost_per_item + processing_fee),
+//        Err(failure) => Err(failure),
+//    }
 }
 
 #[cfg(test)]
@@ -46,3 +49,21 @@ mod tests {
         );
     }
 }
+
+
+
+//enum Result {
+//  Ok(value),
+//  Err(type_of_error)
+//}
+//
+//impl Resut::Ok {
+//  fn unwrap() {
+//    retuern value
+//  }
+//}
+//
+//
+//impl Result::Err {
+//  fn unwrap_err() { return(error_type) }
+//}
