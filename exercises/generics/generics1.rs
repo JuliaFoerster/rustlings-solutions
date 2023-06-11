f912735ed6050c4fca4dbaa1cd5d3bbfd8a2baaa
Julia <juliafoerster86@googlemail.com>
2023-06-11 22:00:48 +0100

    chore: completed exercise errors5 and errors6, generics and traits

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -3,9 +3,22 @@
 
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+struct Basket<T> { 
+    items: Vec<T> 
+} 
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+
+    let mut basket = Basket { items: vec![] };
+
+    basket.items.push("banana");
+    basket.items.push("apples");
+
+
+    let mut basket_julia = Basket { items: vec![] };
+    basket_julia.items.push((1,23));
+
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
