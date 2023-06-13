f052aefdaee8e19ec87509f69f797b17ab7ddbf8
Julia <juliafoerster86@googlemail.com>
2023-06-13 19:56:47 +0100

    chore: completed exercise iterator2

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,8 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function
 // "hello" -> "Hello"
@@ -12,16 +10,28 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.to_uppercase().to_string()+&c.next().unwrap().to_string()+&c.next().unwrap().to_string()+&c.next().unwrap().to_string()+&c.next().unwrap().to_string(),
+        //Some(first) => first.to_uppercase().to_string()+&c.next().unwrap().to_string()+&c.next().unwrap().to_string()+&c.next().unwrap().to_string()+&c.next().unwrap().to_string(),
+        Some(first) => {
+            let mut x = first.to_uppercase().to_string();
+            for input_char in c {
+                x.push(input_char);
+            }
+            x
+        }
     }
 }
 
-// Step 2.
+// Step 2
 // Apply the `capitalize_first` function to a slice of string slices.
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|word| capitalize_first(word)).collect()
+    //let mut words_uppercase = Vec::new();
+    //for i in words {
+    //    words_uppercase.push(capitalize_first(i));
+    //}
+    //words_uppercase
 }
 
 // Step 3.
@@ -29,7 +39,8 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+     words.iter().map(|word| capitalize_first(word)).collect()
+     //words.map(fn (word: &str) { capitalize_first(word) })
 }
 
 #[cfg(test)]
