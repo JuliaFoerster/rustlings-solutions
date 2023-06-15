a9e387805afaa91cfc71553b5cc04ebf40ef7e81
Julia <juliafoerster86@googlemail.com>
2023-06-15 21:37:29 +0100

    chore: competed exercise iterator4

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -13,6 +11,18 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let mut v = 1..=num;
+    v.fold(1,|accum, current| accum*current)
+
+     //   accum * current
+     //   f(x) = x+1
+     //   |x| = x*(x-1)
+     //   0 = 0 *-1 = 0 OK
+     //   1 = 1*0 = 0 FALSE 1
+     //   2 = 2*1 = 2 OK 2
+     //   3 = 3*2 = 6 OK 6
+     //   4 = 4*3 = 12 FALSE 24
+     //   5 = 5*4 = 20 FALSE
 }
 
 #[cfg(test)]
